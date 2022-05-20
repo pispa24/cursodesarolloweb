8a4aed2985319727d3cccc156e8017cb43204774
PC-1\PC1mamadou <mamadou>
2022-05-20 09:32:21 +0200

    proyectos

---
+++ HTML/proyectos/ranking/aboutme.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <?php
+  require_once 'mod/header.php';
+  ?>
+  <link href="css/estilos.css" rel="stylesheet" type="text/css">
+</head>
+  <body>
+    <section class="sectionabout">
+      <div class="divabout"></div>
+      <img  class="imgabout"src="" >
+      <h2 class="h2about">Introducción</h2>
+      <p class="pabout">Mi nombre es </p>
+      <p class="pabout">Me Gust es </p>
+      <br>
+      <h2 class="h2about">De donde soy</h2>
+      <p class="pabout">Naci en un sitio </p>
+
+      <br>
+      <h2 class="h2about">Experiencia profesional</h2>
+      <p class="pabout">Mi nombre es </p>
+      <p class="pabout">Me Gust es </p>
+      <br>
+      <h2 class="h2about">Objetivo de la web</h2>
+      <p class="pabout">Mi nombre es </p>
+      
+
+
+
+    </section>
+
+  </body>
+  <?php
+    require_once 'mod/footer.php';
+  ?>
+</html>
